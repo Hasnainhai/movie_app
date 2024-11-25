62828c5723c45c6db9c924f40bc390366582acc6
Hasnain Haider <codeWithHasnain>
2024-11-25 23:31:30 +0500

    Flutter bloc Favourite App

--- lib/bloc/favouriteApp/favourite_app_events.dart
+++ lib/bloc/favouriteApp/favourite_app_events.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_movie_app/models/favourite_item_model.dart';
 import 'package:equatable/equatable.dart';
 
 abstract class FavouriteAppEvents extends Equatable {
@@ -8,3 +9,20 @@ abstract class FavouriteAppEvents extends Equatable {
 }
 
 class FetchFavouriteEvents extends FavouriteAppEvents {}
+
+class FavouriteItem extends FavouriteAppEvents {
+  const FavouriteItem({required this.favouriteItemModel});
+  final FavouriteItemModel favouriteItemModel;
+}
+
+class SelectItem extends FavouriteAppEvents {
+  const SelectItem({required this.favouriteItemModel});
+  final FavouriteItemModel favouriteItemModel;
+}
+
+class UnSelectItem extends FavouriteAppEvents {
+  const UnSelectItem({required this.favouriteItemModel});
+  final FavouriteItemModel favouriteItemModel;
+}
+
+class DeleteItems extends FavouriteAppEvents {}
--- lib/ui/favourite_app_screen/favourite_app_screen.dart
+++ lib/ui/favourite_app_screen/favourite_app_screen.dart
@@ -1,4 +1,9 @@
+import 'package:bloc_movie_app/bloc/favouriteApp/favourite_app_bloc.dart';
+import 'package:bloc_movie_app/bloc/favouriteApp/favourite_app_events.dart';
+import 'package:bloc_movie_app/bloc/favouriteApp/favourite_app_state.dart';
+import 'package:bloc_movie_app/models/favourite_item_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FavouriteAppScreen extends StatefulWidget {
   const FavouriteAppScreen({super.key});
@@ -9,7 +14,85 @@ class FavouriteAppScreen extends StatefulWidget {
 
 class _FavouriteAppScreenState extends State<FavouriteAppScreen> {
   @override
+  void initState() {
+    context.read<FavouriteAppBloc>().add(FetchFavouriteEvents());
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favourite App'),
+        actions: [
+          BlocBuilder<FavouriteAppBloc, FavouriteAppState>(
+            builder: (context, state) {
+              return Visibility(
+                visible: state.tempFavouriteItemList.isEmpty ? false : true,
+                child: IconButton(
+                    onPressed: () {
+                      context.read<FavouriteAppBloc>().add(DeleteItems());
+                    },
+                    icon: const Icon(Icons.delete, color: Colors.white)),
+              );
+            },
+          ),
+        ],
+      ),
+      body: BlocBuilder<FavouriteAppBloc, FavouriteAppState>(
+          builder: (context, state) {
+        switch (state.listStatus) {
+          case ListStatus.loading:
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          case ListStatus.failure:
+            return const Center(
+              child: Text('error loading favourite List'),
+            );
+          case ListStatus.success:
+            return ListView.builder(
+                itemCount: state.favouriteItemList.length,
+                itemBuilder: (context, index) {
+                  final item = state.favouriteItemList[index];
+
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: ListTile(
+                        leading: Checkbox(
+                            value: state.tempFavouriteItemList.contains(item)
+                                ? true
+                                : false,
+                            onChanged: (value) {
+                              if (value!) {
+                                context
+                                    .read<FavouriteAppBloc>()
+                                    .add(SelectItem(favouriteItemModel: item));
+                              } else {
+                                context.read<FavouriteAppBloc>().add(
+                                    UnSelectItem(favouriteItemModel: item));
+                              }
+                            }),
+                        title: Text(item.value.toString()),
+                        trailing: IconButton(
+                            onPressed: () {
+                              FavouriteItemModel itemModel = FavouriteItemModel(
+                                  id: item.id,
+                                  value: item.value,
+                                  isFavourite: item.isFavourite ? false : true);
+                              context.read<FavouriteAppBloc>().add(
+                                  FavouriteItem(favouriteItemModel: itemModel));
+                            },
+                            icon: Icon(item.isFavourite
+                                ? Icons.favorite
+                                : Icons.favorite_border)),
+                      ),
+                    ),
+                  );
+                });
+        }
+      }),
+    );
   }
 }
